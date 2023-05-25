9c16d11f53c95b4be912f9a39c17f293bf9dc0ca
ogbozoyan <oganes.bozoyan@mail.ru>
2023-05-25 19:37:21 +0300

    feat: changed inMem to JDBC AuthenticationManagerBuilder

--- src/main/java/com/example/spring_security/configuration/application/SecurityConfiguration.java
+++ src/main/java/com/example/spring_security/configuration/application/SecurityConfiguration.java
@@ -1,12 +1,16 @@
 package com.example.spring_security.configuration.application;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 /**
  * @author ogbozoyan
@@ -14,6 +18,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  */
 @EnableWebSecurity // tells this is to Spring Security configuration that take this class like a configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    /* From Memory
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
@@ -24,6 +29,27 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .withUser("user")
                 .password("user")
                 .roles("USER");
+    }*/
+
+    //From Database
+    @Autowired
+    DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .withDefaultSchema()
+                .withUser(
+                        User.withUsername("user")
+                                .password("user")
+                                .roles("USER")
+                )
+                .withUser
+                        (User.withUsername("admin")
+                                .password("admin")
+                                .roles("ADMIN")
+                        );
     }
 
     @Override
