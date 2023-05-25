8fad6bfe169493bd0970ba4a23203d9200f9b869
ogbozoyan <oganes.bozoyan@mail.ru>
2023-05-25 19:43:13 +0300

    feat: create manually schema and users

--- src/main/java/com/example/spring_security/configuration/application/SecurityConfiguration.java
+++ src/main/java/com/example/spring_security/configuration/application/SecurityConfiguration.java
@@ -6,7 +6,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -38,18 +37,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication()
-                .dataSource(dataSource)
-                .withDefaultSchema()
-                .withUser(
-                        User.withUsername("user")
-                                .password("user")
-                                .roles("USER")
-                )
-                .withUser
-                        (User.withUsername("admin")
-                                .password("admin")
-                                .roles("ADMIN")
-                        );
+                .dataSource(dataSource);
     }
 
     @Override
