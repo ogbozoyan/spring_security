a0ef515b8c4e5789614c384055f716c584b83054
ogbozoyan <oganes.bozoyan@mail.ru>
2023-05-25 15:41:00 +0300

    Initial commit

---
+++ src/main/java/com/example/spring_security/web/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.example.spring_security.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ogbozoyan
+ * @date 25.05.2023
+ */
+@RestController("/")
+public class HomeController {
+
+    @GetMapping
+    public String index() {
+        return "Index page";
+    }
+}
