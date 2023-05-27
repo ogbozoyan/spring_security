6e1bfbb91afe798af8f98524d8bd035b5ec29886
ogbozoyan <oganes.bozoyan@mail.ru>
2023-05-27 12:53:21 +0300

    add: simple ldap authentication

--- src/main/java/com/example/spring_security/configuration/application/SecurityConfiguration.java
+++ src/main/java/com/example/spring_security/configuration/application/SecurityConfiguration.java
@@ -6,8 +6,11 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.ldap.EmbeddedLdapServerContextSourceFactoryBean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.ldap.server.UnboundIdContainer;
 
 import javax.sql.DataSource;
 
@@ -17,40 +20,33 @@ import javax.sql.DataSource;
  */
 @EnableWebSecurity // tells this is to Spring Security configuration that take this class like a configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    /* From Memory
+
+    //ben benspassword
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("admin")
-                .password("admin")
-                .roles("ADMIN", "USER")
+        auth
+                .ldapAuthentication()
+                .userDnPatterns("uid={0},ou=people")
+                .groupSearchBase("ou=groups")
+                .contextSource()
+                .url("ldap://localhost:8389/dc=springframework,dc=org")
                 .and()
-                .withUser("user")
-                .password("user")
-                .roles("USER");
-    }*/
-
-    //From Database
-    @Autowired
-    DataSource dataSource;
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication()
-                .dataSource(dataSource);
+                .passwordCompare()
+                .passwordEncoder(new BCryptPasswordEncoder())
+                .passwordAttribute("userPassword");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/admin").hasRole("ADMIN") // only admin can access to this page
-                .antMatchers("/user").hasAnyRole("USER", "ADMIN") // only user and admin can access to this page
-                .antMatchers("/").permitAll()
-                .and().formLogin();// everyone can access to this page
+                .anyRequest().fullyAuthenticated()
+                .and()
+                .formLogin();// everyone can access to this page
     }
 
     @Bean
     PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance(); //Not recommended
     }
+
 }
